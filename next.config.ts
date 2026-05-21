666b3ad84b5b3628a1de93c6f438887445f0d6bd
keirsalterego <darksalterego99@gmail.com>
2026-05-21 20:44:22 +0530

    Remove basePath and assetPrefix from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/kerox-www',     
-  assetPrefix: '/kerox-www/',  
 };
 
 export default nextConfig;
