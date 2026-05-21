233955f6333837695ba143c22ecd4071766aadb6
keirsalterego <darksalterego99@gmail.com>
2026-05-21 20:28:13 +0530

    Update next.config.ts with export settings and base path configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/kerox-www',     
+  assetPrefix: '/kerox-www/',  
 };
 
 export default nextConfig;
